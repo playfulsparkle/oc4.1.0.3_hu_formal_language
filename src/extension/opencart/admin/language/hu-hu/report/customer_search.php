873a8a963eb635052860bdbf11f8227bb6926532
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2025-10-18 16:06:07 +0200

    Customer Searches Report translation fix

--- src/extension/opencart/admin/language/hu-hu/report/customer_search.php
+++ src/extension/opencart/admin/language/hu-hu/report/customer_search.php
@@ -1,31 +1,31 @@
 <?php
 // Heading
-$_['heading_title']     = 'Customer Searches Report';
+$_['heading_title']     = 'Vásárlói keresések jelentés';
 
 // Text
-$_['text_extension']    = 'Extensions';
-$_['text_edit']         = 'Edit Customer Searches Report';
-$_['text_success']      = 'Success: You have modified customer searches report!';
-$_['text_filter']       = 'Filter';
-$_['text_guest']        = 'Guest';
+$_['text_extension']    = 'Bővítmények';
+$_['text_edit']         = 'Vásárlói keresések jelentés szerkesztése';
+$_['text_success']      = 'Siker: A vásárlói keresések jelentés sikeresen módosítva lett!';
+$_['text_filter']       = 'Szűrő';
+$_['text_guest']        = 'Vendég';
 $_['text_customer']     = '<a href="%s">%s</a>';
 
 // Column
-$_['column_keyword']    = 'Keyword';
-$_['column_products']   = 'Found Products';
-$_['column_category']   = 'Category';
-$_['column_customer']   = 'Customer';
-$_['column_ip']         = 'IP';
-$_['column_date_added'] = 'Date Added';
+$_['column_keyword']    = 'Keresőszó';
+$_['column_products']   = 'Talált termékek';
+$_['column_category']   = 'Kategória';
+$_['column_customer']   = 'Vásárló';
+$_['column_ip']         = 'IP cím';
+$_['column_date_added'] = 'Hozzáadás dátuma';
 
 // Entry
-$_['entry_date_start']  = 'Date Start';
-$_['entry_date_end']    = 'Date End';
-$_['entry_keyword']     = 'Keyword';
-$_['entry_customer']    = 'Customer';
-$_['entry_ip']          = 'IP';
-$_['entry_status']      = 'Status';
-$_['entry_sort_order']  = 'Sort Order';
+$_['entry_date_start']  = 'Kezdés dátuma';
+$_['entry_date_end']    = 'Befejezés dátuma';
+$_['entry_keyword']     = 'Keresőszó';
+$_['entry_customer']    = 'Vásárló';
+$_['entry_ip']          = 'IP cím';
+$_['entry_status']      = 'Állapot';
+$_['entry_sort_order']  = 'Sorrend';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify customer searches report!';
+$_['error_permission']  = 'Figyelmeztetés: Nincs jogosultsága a vásárlói keresések jelentés módosításához!';
