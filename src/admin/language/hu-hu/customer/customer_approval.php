71b83705886dccce7869a4aaadcfe5f1cfcadf77
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2025-10-18 15:46:16 +0200

    translation update

--- src/admin/language/hu-hu/customer/customer_approval.php
+++ src/admin/language/hu-hu/customer/customer_approval.php
@@ -1,10 +1,10 @@
 <?php
 // Heading
-$_['heading_title']         = 'Vásárlói jogosultságok';
+$_['heading_title']         = 'Vásárlói jóváhagyások';
 
 // Text
-$_['text_success']          = 'Siker: A vásárlói jogosultság sikeresen módosítva lett!';
-$_['text_list']             = 'Vásárlói jogosultság lista';
+$_['text_success']          = 'Siker: Sikeresen módosította a vásárlói jóváhagyásokat!';
+$_['text_list']             = 'Vásárlói jóváhagyások listája';
 $_['text_default']          = 'Alapértelmezett';
 $_['text_customer']         = 'Vásárló';
 $_['text_affiliate']        = 'Partner';
@@ -13,15 +13,15 @@ $_['text_approve']          = 'Jóváhagyás';
 $_['text_deny']             = 'Elutasítás';
 
 // Column
-$_['column_customer']       = 'Vásárló';
+$_['column_customer']       = 'Vásárló neve';
 $_['column_email']          = 'E-mail';
 $_['column_customer_group'] = 'Vásárlói csoport';
 $_['column_type']           = 'Típus';
-$_['column_date_added']     = 'Hozzáadva';
+$_['column_date_added']     = 'Hozzáadás dátuma';
 $_['column_action']         = 'Művelet';
 
 // Entry
-$_['entry_customer']        = 'Vásárló';
+$_['entry_customer']        = 'Vásárló neve';
 $_['entry_email']           = 'E-mail';
 $_['entry_customer_group']  = 'Vásárlói csoport';
 $_['entry_type']            = 'Típus';
@@ -29,4 +29,4 @@ $_['entry_date_from']       = 'Dátumtól';
 $_['entry_date_to']         = 'Dátumig';
 
 // Error
-$_['error_permission']      = 'Figyelmeztetés: Nincs jogosultsága a vásárlói jogosultságok módosításához!';
+$_['error_permission']      = 'Figyelmeztetés: Nincs jogosultsága a vásárlói jóváhagyások módosításához!';
